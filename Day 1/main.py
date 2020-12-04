0bfd2cc24ddfd7ba63793f35ad13aace6e687892
aParr0t <a.lapenas04@gmail.com>
2020-12-04 12:11:29 +0100

    Finished Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,33 @@
+# read the input
+inputFile = open("input.txt", "r")
+nums = inputFile.readlines()
+
+for i in range(len(nums)):
+    nums[i] = int(nums[i].rstrip("\n"))
+
+#Part-1
+
+for num in nums:
+    rest = 2020-num
+    if rest in nums:
+        answer = num * nums[nums.index(rest)]
+
+print(answer)
+
+
+# # Part-2
+#
+# rests = []
+# for i in range(len(nums)):
+#     rests.append(2020-nums[i])
+#
+# answer = False
+# for rest in rests:
+#     for num in nums:
+#         rest2 = rest-num
+#         if rest2 in nums:
+#             answer = nums[rests.index(rest)] * num * rest2
+#             break
+#     if answer:
+#         break
+# print(answer)
