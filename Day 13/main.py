541de1d0d63cc06289cf3207bef029e134b162a2
aParr0t <a.lapenas04@gmail.com>
2020-12-15 20:37:04 +0100

    Finished Day 13

--- Day 13/main.py
+++ Day 13/main.py
@@ -1,6 +1,4 @@
-import time
-start_time = time.time()
-
+import math
 # read the input file
 inputFile = open("input.txt", "r")
 puzzleInput = inputFile.readlines()
@@ -23,8 +21,7 @@ for i in range(len(puzzleInput)):
 # # Part One
 
 
-# Part Two
-# My sollution - aka brute force
+# Part Two - My sollution - aka brute force
 # delays = []
 # busses = [x for x in puzzleInput[1].split(",")]
 # for idx, item in enumerate(busses):
@@ -54,14 +51,50 @@ for i in range(len(puzzleInput)):
 # # 11546953709433246720
 # print("--- %s seconds ---" % (time.time() - start_time))
 
-# Part Two
-# Chinese remainder theorem
+# Part Two - Chinese remainder theorem
+def modInverse(a, m): 
+    m0 = m 
+    y = 0
+    x = 1
+  
+    if (m == 1): 
+        return 0
+  
+    while (a > 1): 
+  
+        # q is quotient 
+        q = a // m 
+  
+        t = m 
+  
+        # m is remainder now, process 
+        # same as Euclid's algo 
+        m = a % m 
+        a = t 
+        t = y 
+  
+        # Update x and y 
+        y = x - q * y 
+        x = t 
+  
+    # Make x positive 
+    if (x < 0): 
+        x = x + m0 
+  
+    return x
 
-def modularInverse(a, m):
-    x, y = a, m
-    while x-1 != y:
-        x += a
-        if x-y > 1:
-            y += m
-    return x//a
-print(modularInverse(10, 17))
+delays = []
+busses = [x for x in puzzleInput[1].split(",")]
+for idx, item in enumerate(busses):
+    if item != "x":
+        delays.append(idx)
+busses = [int(x) for x in puzzleInput[1].split(",") if x != "x"]
+
+
+rem = [busses[x]-delays[x] for x in range(len(busses))]
+product = math.prod(busses)
+pp = [product//busses[x] for x in range(len(busses))]
+inv = [modInverse(pp[x], busses[x]) for x in range(len(busses))]
+mult = [pp[x]*rem[x]*inv[x] for x in range(len(busses))]
+t = sum(mult) % product
+print(t)
